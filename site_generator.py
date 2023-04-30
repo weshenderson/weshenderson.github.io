26c6ff5dff47e7b4b91742fe0a8e53c4e4a37e0a
Wes Henderson <whendersonii@gmail.com>
2023-04-30 17:19:17 -0500

    Adding a site_generator script and templitizing my site.

---
+++ site_generator.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import yaml
+from string import Template
+
+with open('content.yaml') as f:
+    content = yaml.safe_load(f)
+
+site_content = {}
+source       = "index.tmpl"
+destination  = "index.html"
+
+# Generate the header.
+for title in content["Header"]:
+    site_content['header'] = content["Header"][title]
+
+# Generate the body.
+site_content['body'] = ''
+for link in content["Body"]:
+    site_content['body'] += '<a target="_blank" href="' + content["Body"][link] + '">' + link + '</a><br>'
+
+# Generate the footer.
+site_content['college'] = "529 Donation Links: "
+count = 1
+
+# Testing len()
+for child in content["Footer"][529]:
+    if count != len(content["Footer"][529]):
+        site_content['college'] += '<a target="_blank" href="' + content["Footer"][529][child] + '">' + child + '</a>' + content["Footer"]["FS"]
+    else:
+        site_content['college'] += '<a target="_blank" href="' + content["Footer"][529][child] + '">' + child + '</a>'
+    count += 1
+    site_content['copyright'] = content["Footer"]["Copyright"]
+
+# Build index.html
+if os.path.exists(destination):
+    os.remove(destination)
+with open(source, 'r') as f:
+    src    = Template(f.read())
+    result = src.substitute(site_content)
+    with open(destination, 'a+') as d:
+        d.write(result)
+
+os.chmod(destination, 0o755)
